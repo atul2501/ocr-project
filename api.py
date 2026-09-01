b096280294c1d87b78bdcd97b86e6006a682c52b
Atul yadav <57094600+atul2501@users.noreply.github.com>
2026-09-01 17:14:43 +0530

    Replace API keys with a new key

--- api.py
+++ api.py
@@ -18,19 +18,7 @@ SAVE_DEBUG_PAGES = False
 
 
 API_KEYS = [
-            '7fa7e90e49af4eacb59a86724d3aa455._nKylCiXeshF-AP-jMr2cCrg',
-            # 'a02fc9d9ca954e8f8e801031d741a9f4.8YkE0poV42BUpJM5aSdcpERx',
-            # '5c142e3dddbe4c00af461beb686cfd36.qA5pDYNm0V79DCnjL7mXV6Gy',
-            # '08cd689726cb40169543db1ac28fe0e2.silxzK-S0NjozIFgWon0Gyx9',
-            # 'bfb4bf4cdef8469c8841ba02e1d30b0b.dzIH64NMy-F0m6AxZ1h9mB_q',
-            # 'b3e6113f430b40adb69af4250e92013e.V3erJlkPIvn5J_Hnh_whesfJ',
-            # 'cc76568fb2e040988cf13671da7b9d19.RcuniqYYf7DpS8xqgeOP1gRt',
-            # 'dd633d6cc5754397a5fe335fce8e7e8a.xV0UoGbMFC2xP332Y-Q2rlme',
-            # '84a4197f524a430188e998fd9255f41b.6Dwy42j1n653P0OlOoPQovmK',
-            # '0e79c0347e2945428054bb16a743db0f.exH-Kxg7zCG7PUrlO7-KxpFi',
-            # '59d963bc579f4d9b8020c739dadbf798.sLp5W442w8ctn4KptTzcT5TU',
-            # '05bdcd9506cf413995102efc4c27a2ca.1b8-iPCwnBg1lJmccND9w0YZ',
-            # 'a96c2d79ded643caa217bbc0c185796d.-R6IC1WvWzARUphYcpMZ5mUJ',  
+            '7f87db1688e44289a37ee3d1d732a95d.n9WONLII6kxQYTw0RupWC8Ce',
             ]
 
 MAX_WORKERS = 10 
